e8a0217e25f922a4d15ee2ede1e3bfe5d92d1caa
Avinash Gupta <agupta30@gmail.com>
2016-11-19 21:00:40 -0800

    Added Stress Test for JMeter

---
+++ Testing/src/main/java/com/avinash/learn/StringUtilsStressTest.java
@@ -0,0 +1,47 @@
+package com.avinash.learn;
+
+import java.io.Serializable;
+
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+
+public class StringUtilsStressTest extends AbstractJavaSamplerClient implements Serializable {
+
+	private static final String METHOD_TAG = "method";
+	private static final String ARG1_TAG = "inputStr";
+
+	private static final long serialVersionUID = -4537358620199171581L;
+
+	@Override
+	public Arguments getDefaultParameters() {
+		Arguments defaultParameters = new Arguments();
+		defaultParameters.addArgument(METHOD_TAG, "reverse");
+		defaultParameters.addArgument(ARG1_TAG, "Avinash");
+
+		return defaultParameters;
+	}
+
+	@Override
+	public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
+
+		SampleResult sampleResult = new SampleResult();
+		sampleResult.sampleStart();
+
+		try {
+			String message = StringUtils.reverse(ARG1_TAG);
+			sampleResult.sampleEnd();
+			sampleResult.setSuccessful(Boolean.TRUE);
+			sampleResult.setResponseCodeOK();
+			sampleResult.setResponseMessage(message);
+		} catch (Exception e) {
+			sampleResult.sampleEnd();
+			sampleResult.setResponseMessage(e.getMessage());
+			sampleResult.setSuccessful(Boolean.FALSE);
+		}
+
+		return sampleResult;
+	}
+
+}
